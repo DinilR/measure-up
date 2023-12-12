f9dbed33d1f92f44f93320a6b4b966dec34c67b9
DinilR <dinilsratnayake@gmail.com>
2023-12-12 23:38:29 +0530

    created intro section frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Appbar from "./Components/Appbar";
+import IntroSection from "./Components/IntroSection";
 
 function App() {
   return (
     <>
       <Appbar />
+      <IntroSection />
       <h1>Hi this is measure up</h1>
     </>
   );
---
+++ src/Components/IntroSection.js
@@ -0,0 +1,89 @@
+import { Box, Typography,Grid } from "@mui/material";
+import product from "./../Resources/product.png";
+import logo from "./../Resources/MLogo.png";
+import { styled } from "@mui/system";
+
+const IntroSection = () => {
+  const ResponsiveBox = styled(Box)(({ theme }) => ({
+    
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: 'center',
+    },
+  }));
+
+  return (
+    <>
+      <ResponsiveBox
+        sx={{
+          bgcolor: "#000000",
+          display: "flex",
+          flexDirection: "row",
+          color: "white",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Grid container spacing={2}>
+          {/* Content Section */}
+          <Grid item xs={12} md={6} sx={{mt:"5%"}}>
+            <Box
+              className="content_section"
+              sx={{
+                mb: "3%",
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                textAlign: 'center',
+                ml:"2%",
+                mr:"2%"
+              }}
+            >
+              <Typography variant="h3" sx={{textAlign: 'center'}} gutterBottom>
+                Measure up
+              </Typography>
+              <Box  sx={{textAlign: 'left', ml:"5%"}}>
+              <Typography variant="body1" gutterBottom>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco.
+              </Typography>
+              <Typography variant="body1" sx={{textAlign: 'left'}} gutterBottom>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco.
+              </Typography>
+              </Box>
+            </Box>
+          </Grid>
+
+          {/* Picture Section */}
+          <Grid item xs={12} md={6} sx={{mt:"4%"}}>
+            <Box
+              className="picture_section"
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                mb:"10%"
+              }}
+            >
+              <img
+                src={product}
+                alt="Logo"
+                className="product"
+                style={{
+                  width: "340px",
+                  height: "250px",
+                  marginRight: "10px",
+                  mt: "50px",
+                }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </ResponsiveBox>
+    </>
+  );
+};
+
+export default IntroSection;
