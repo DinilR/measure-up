c1a747666d1c4f8d2e09033805706025c6268c40
DinilR <dinilsratnayake@gmail.com>
2023-12-27 12:10:18 +0530

    email sending alert frontend

--- src/Components/SupportSection.js
+++ src/Components/SupportSection.js
@@ -1,7 +1,20 @@
-import { Box, Button, TextField, Typography, styled } from "@mui/material";
-import React, { useRef } from "react";
+import {
+  Alert,
+  Box,
+  Button,
+  Collapse,
+  IconButton,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import React, { useRef, useState } from "react";
 import background from "./../Resources/background_support_2.png";
 import emailjs from "@emailjs/browser";
+import LoadingButton from "@mui/lab/LoadingButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SupportSection = () => {
   const PictureSection = styled(Box)(({ theme }) => ({
@@ -17,6 +30,9 @@ const SupportSection = () => {
     },
   }));
 
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(true);
+
   const form = useRef();
 
   const serviceId = "service_p2llj2x";
@@ -27,15 +43,31 @@ const SupportSection = () => {
   const sendEmail = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       console.log("In sending mail");
       emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
         (result) => {
           console.log(result.text);
+          setLoading(false);
+          toast.success(
+            "Thank you for your time, Our team will be in touch with you shortly :)",
+            {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            }
+          );
         },
         (error) => {
           console.log(error.text);
         }
       );
+      // setLoading(false);
+      console.log("Send successful");
     } catch (error) {
       console.error("Error sending email:", error);
     }
@@ -47,6 +79,48 @@ const SupportSection = () => {
         sx={{ display: "flex", flexDirection: "row", background: "#E6FAE6" }}
         id="support"
       >
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          style={{ width: "580px" }}
+        />
+        <Box
+          sx={{
+            mb: 2,
+            zIndex: 1,
+            position: "fixed",
+            left: "50%",
+            transform: "translateX(-50%)",
+            width: "80%",
+          }}
+          className="alert"
+        >
+          <Collapse in={open} sx={{}}>
+            <Alert
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+            >
+              Thank you for your time, our team will contact you soon!
+            </Alert>
+          </Collapse>
+        </Box>
         <ContentSection sx={{ width: "50%", ml: "3%", mt: "10%" }}>
           <Typography variant="h5" sx={{ fontWeight: 800 }} gutterBottom>
             Interested in trying Measure Up?
@@ -73,6 +147,7 @@ const SupportSection = () => {
                   sx={{ width: "95%", mt: "0px" }}
                   name="name"
                   type="text"
+                  defaultValue=""
                 />
               </Box>
               <Box sx={{ mt: "2%" }}>
@@ -92,6 +167,7 @@ const SupportSection = () => {
                   sx={{ width: "95%", mt: "0px" }}
                   name="email"
                   type="email"
+                  defaultValue=""
                 />
               </Box>
               <TextField
@@ -105,14 +181,15 @@ const SupportSection = () => {
                 }}
               />
               <Box sx={{ mt: "5%" }}>
-                <Button
+                <LoadingButton
+                  loading={loading}
                   variant="contained"
                   color="success"
                   type="submit"
                   value="Send"
                 >
                   Submit
-                </Button>
+                </LoadingButton>
               </Box>
             </form>
           </Box>
